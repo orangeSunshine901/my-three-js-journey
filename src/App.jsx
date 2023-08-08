a6977c21d5d4ba0d604cae07f2ab762a8c79199e
Abdul Latif <latif901@hotmail.com>
2023-08-08 23:09:29 +0400

    This is my final commit for the lesson

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
     <>
       <Leva collapsed />
       <Canvas
-        dpr={[1, 2]}
+        flat //(Tone Mapping)
+        dpr={[1, 2]} //Pixel Ratio an array can also be passed to limit the range like [1,2]
         gl={{
           toneMapping: THREE.CineonToneMapping, //Tone Mapping default
           outputColorSpace: THREE.SRGBColorSpace, //Output Encoding
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,102 +1,89 @@
-import { useState, useRef, useEffect, Suspense } from "react";
 import {
+  Sparkles,
+  useGLTF,
   OrbitControls,
-  Text3D,
+  useTexture,
   Center,
-  useMatcapTexture,
+  shaderMaterial,
 } from "@react-three/drei";
-import { Perf } from "r3f-perf";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
 import * as THREE from "three";
-import { useFrame } from "@react-three/fiber";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-//// Second way to avoid creating multiple instances of geometry, repeating ourselves, and to optimize.
-const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
-const material = new THREE.MeshMatcapMaterial();
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#FFFFFF"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader,
+);
+
+extend({ PortalMaterial });
 
 export default function Experience() {
-  // One way to avoid creating multiple instances of geometry, repeating ourselves, and to optimize.
-  // const [torusGeometry, setTorusGeometry] = useState();
-  // const [material, setMaterial] = useState();
+  const portalRef = useRef();
 
-  // Solution One for animating the elements by grouping and using it's children
-  // const donutsGroup = useRef();
+  const { nodes } = useGLTF("./model/portal.glb");
 
-  // Solution Two for animating the elements by grouping and using it's children
-  const donuts = useRef([]);
-
-  const [matcapTexture] = useMatcapTexture("617586_23304C_1B1E30_4988CF", 256);
-  // console.log(metcapTexture);
-
-  // Second way to avoid creating multiple instances of geometry, repeating ourselves, and to optimize.
-  useEffect(() => {
-    matcapTexture.colorSpace = THREE.SRGBColorSpace;
-    material.needsUpdate = true;
-
-    material.matcap = matcapTexture;
-    material.needsUpdate = true;
-  }, []);
+  const bakedTexture = useTexture("./model/baked.jpg");
+  bakedTexture.flipY = false;
 
   useFrame((state, delta) => {
-    // Solution One for animating the elements by grouping and using it's children
-    // for (const donut of donutsGroup.current.children) {
-    //   donut.rotation.y += delta * 0.8;
-    // }
-    for (const donut of donuts.current) {
-      donut.rotation.y += delta * 0.8;
-    }
+    portalRef.current.uTime += delta * 20;
   });
 
   return (
     <>
-      <Perf position="top-left" />
       <OrbitControls enableDamping={true} makeDefault />
       <directionalLight position={[1, 2, 3]} color="pink" intensity={1.9} />
       <ambientLight intensity={0.1} />
 
-      {/* To avoid creating multiple instances of geometry, repeating ourselves,
-      and to optimize. */}
-      {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
-      <meshMatcapMaterial ref={setMaterial} matcap={metcapTexture} /> */}
-
-      <Suspense>
-        {/* 
-        // Solution One for animating the elements by grouping and using it's children
-        <group ref={donutsGroup}> */}
-        {[...Array(100)].map((item, index) => (
-          <mesh
-            ref={(el) => (donuts.current[index] = el)}
-            geometry={torusGeometry}
-            material={material}
-            key={index}
-            position={[
-              (Math.random() - 0.5) * 10,
-              (Math.random() - 0.5) * 10,
-              (Math.random() - 0.5) * 10,
-            ]}
-            scale={0.2 + Math.random() * 0.2}
-            rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-          >
-            //TorusGeometry(radius : Float, tube : Float, radialSegments :
-            Integer, tubularSegments : Integer, arc : Float)
-          </mesh>
-        ))}
-        {/* </group> */}
-      </Suspense>
       <Center>
-        <Text3D
-          font="./fonts/helvetiker_regular.typeface.json"
-          size={0.75}
-          height={0.2}
-          curveSegments={12}
-          bevelEnabled
-          bevelThickness={0.02}
-          bevelSize={0.02}
-          bevelOffset={0}
-          bevelSegments={5}
-          material={material}
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        ></mesh>
+
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        ></mesh>
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
         >
-          Hello You!
-        </Text3D>
+          {/* 
+          Standard way to apply vertex and fragment shader material
+
+          <shaderMaterial
+            vertexShader={portalVertexShader}
+            fragmentShader={portalFragmentShader}
+            uniforms={{
+              uTime: { value: 0 },
+              uColorStart: { value: new THREE.Color("#ffffff") },
+              uColorEnd: { value: new THREE.Color("#000000") },
+            }}
+          /> */}
+          <portalMaterial ref={portalRef} />
+        </mesh>
+
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
       </Center>
     </>
   );
